6f1627dde02cbfe58c6128bc9b6e0dc5d85a39c2
Lasertype <49509778+Lasertype@users.noreply.github.com>
2019-12-10 10:29:37 -0800

    Update GameContainer.jsx

--- src/Components/GameContainer/GameContainer.jsx
+++ src/Components/GameContainer/GameContainer.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import "./GameContainer.scss";
+import GameBar from "../../GameBar/GameBar.jsx";
 
 const GameContainer = () => {
     return (
         <div className="masterGameContainer"></div>
+        <GameBar />
     )
 }
 
