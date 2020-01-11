0e4c8706aeabb008e09f200ccade3fc327bb55a7
Lasertype <dlasezkay@gmail.com>
2020-01-10 21:04:42 -0800

    implemented master encounter control switch; began working on party modal but will continue with that on a new branch; goal of this branch was to get master switch operational

--- src/Components/GameContainer/GameContainer.jsx
+++ src/Components/GameContainer/GameContainer.jsx
@@ -7,12 +7,27 @@ import PlayerCreation from '../PlayerCreation/PlayerCreation';
 const GameContainer = () => {
     const [modal, setModal] = useState(false); 
     const [players, setPlayers] = useState([]);
+    const [master, setMaster] = useState(0)
         
-    return (
+    const renderMaster = masterValue => {
+    switch (masterValue) {
+      case 0:
+        return <Background />;
+      case 1:
+        return <PlayerCreation />;
+      default:
+        return <GameBar />;
+    }
+}
+
+return (
         <React.Fragment>
-            <Background />
-            <GameBar modal={modal} setModal={setModal} players={players} />
+            <div className="masterRender">
+                {renderMaster(master)}
+            </div>
+            <GameBar modal={modal} setModal={setModal} players={players} master={master} setMaster={setMaster}/>
             <PlayerCreation modal={modal} setModal={setModal} players={players} setPlayers={setPlayers} />
+            
         </React.Fragment>
     )
 }
